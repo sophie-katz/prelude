81437ea6863aac0fb08973a989064cde5338822c
Sophie Katz <sophie@sophiekatz.us>
2023-02-12 14:05:47 +0000

    Close #5

--- client/src/pages/IndexPage.vue
+++ client/src/pages/IndexPage.vue
@@ -1,16 +1,16 @@
 <template>
   <q-page>
-    <under-construction-small />
+    <under-construction-large />
   </q-page>
 </template>
 
 <script lang="ts">
 // import { Todo, Meta } from 'components/models';
-import UnderConstructionSmall from '../components/UnderConstructionSmall.vue';
+import UnderConstructionLarge from '../components/UnderConstructionLarge.vue';
 import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'IndexPage',
-  components: { UnderConstructionSmall }
+  components: { UnderConstructionLarge }
 });
 </script>
