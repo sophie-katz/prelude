ff0a04c7540ff729cb5ae8eae478a5402679cfa2
Sophie Katz <sophie@sophiekatz.us>
2023-02-12 13:33:21 +0000

    Close #4

---
+++ client/src/components/UnderConstructionSmall.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="row items-center">
+        <q-img
+            src="../assets/under-construction-64.png"
+            spinner-color="black"
+            class="col-1 q-ma-xs"
+            style="max-width: 1rem; max-height: 1rem;" />
+
+        <div class="col-auto">
+            Under construction
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "UnderConstructionSmall"
+});
+</script>
--- client/src/pages/IndexPage.vue
+++ client/src/pages/IndexPage.vue
@@ -1,16 +1,16 @@
 <template>
-  <q-page class="row">
-    <under-construction-large />
+  <q-page>
+    <under-construction-small />
   </q-page>
 </template>
 
 <script lang="ts">
 // import { Todo, Meta } from 'components/models';
-import UnderConstructionLarge from '../components/UnderConstructionLarge.vue';
+import UnderConstructionSmall from '../components/UnderConstructionSmall.vue';
 import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'IndexPage',
-  components: { UnderConstructionLarge }
+  components: { UnderConstructionSmall }
 });
 </script>
