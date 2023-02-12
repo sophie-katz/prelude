8872c256a544ccf74f74bbef263c2870100048ab
Sophie Katz <sophie@sophiekatz.us>
2023-02-12 13:55:42 +0000

    Close #7

---
+++ client/src/pages/DeployPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <q-page>
+      <under-construction-large />
+    </q-page>
+  </template>
+
+  <script lang="ts">
+  import UnderConstructionLarge from '../components/UnderConstructionLarge.vue';
+  import { defineComponent, ref } from 'vue';
+
+  export default defineComponent({
+    name: 'DeployPage',
+    components: { UnderConstructionLarge }
+  });
+  </script>
--- client/src/router/routes.ts
+++ client/src/router/routes.ts
@@ -4,7 +4,15 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'dashboard', component: () => import('pages/DashboardPage.vue') },
+      { path: 'deploy', component: () => import('pages/DeployPage.vue') },
+      { path: 'document', component: () => import('pages/DocumentPage.vue') },
+      { path: 'search', component: () => import('pages/SearchPage.vue') },
+      { path: 'code', component: () => import('pages/CodePage.vue') },
+      { path: 'ticket', component: () => import('pages/TicketPage.vue') }
+    ],
   },
 
   // Always leave this as last one,
