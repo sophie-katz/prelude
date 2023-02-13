0d6d2c8b4b675854bbcf8d0cdab4698905c1a31e
Sophie Katz <sophie@sophiekatz.us>
2023-02-13 12:47:51 +0000

    Starting on a couple things, switching to authentication

---
+++ client/src/components/SystemButtonSmall.vue
@@ -0,0 +1,11 @@
+<template>
+    <!-- <q-btn -->
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "UnderConstructionLarge"
+});
+</script>
