3532ebbc565971178ce4083291c19b1dbf62aed7
Sophie <sophie@sophiekatz.us>
2023-02-23 12:34:42 +0000

    Integrating stoplight

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4NjU2OA/spectral.js?branch_slug=main&token=7b8d481a-9ce6-4fff-b031-5f71489c2b02";
+export default { extends: ruleset };
