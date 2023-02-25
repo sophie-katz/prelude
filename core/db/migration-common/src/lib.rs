0dab504f7c9c8116258492f74ed7c584be2d4f0b
Sophie Katz <sophie@sophiekatz.us>
2023-02-25 19:56:35 +0000

    Standardizing auditing

--- core/db/migration-common/src/lib.rs
+++ core/db/migration-common/src/lib.rs
@@ -22,7 +22,10 @@
 
 //! This module contains any common code for the database migrations.
 
-use sea_query::Iden;
+#![feature(iter_intersperse)]
+
+use sea_orm_migration::{prelude::SchemaManager, sea_orm::ConnectionTrait, DbErr};
+use sea_query::{ColumnDef, Iden, IntoTableRef, Table, TableCreateStatement};
 use std::collections::HashSet;
 use strum::IntoEnumIterator;
 
@@ -95,14 +98,18 @@ pub fn assert_audit_table_enum_valid<
             x.unquoted(&mut result);
             result
         })
+        .filter(|x| x != table_name_audit)
         .collect::<HashSet<String>>();
 
     // Assert that the audit table contains all the source table identifiers
-    for source_table_iden in SourceTableEnum::iter().map(|x| -> String {
-        let mut result = String::new();
-        x.unquoted(&mut result);
-        result
-    }) {
+    for source_table_iden in SourceTableEnum::iter()
+        .map(|x| -> String {
+            let mut result = String::new();
+            x.unquoted(&mut result);
+            result
+        })
+        .filter(|x| x != table_name_source)
+    {
         assert!(audit_table_iden_set.contains(&source_table_iden));
     }
 
@@ -113,8 +120,254 @@ pub fn assert_audit_table_enum_valid<
     }
 
     // Assert that the audit table contains no extra identifiers
+    //
+    // NOTE: Subtract one to account for the filtering out of the table name
     assert_eq!(
         audit_table_iden_set.len(),
-        SourceTableEnum::iter().count() + AUDIT_TABLE_COLUMNS.len()
+        SourceTableEnum::iter().count() - 1 + AUDIT_TABLE_COLUMNS.len()
     );
+}
+
+/// An enum to identify whether a table is an audit or source table.
+pub enum TableKind {
+    /// Identifies a source table
+    Source,
+    /// Identifies an audit table
+    Audit,
+}
+
+/// Helper function to create an audited table.
+///
+/// # Arguments
+///
+/// * `manager` - A schema manager referenced from the SeaORM migration.
+/// * `columns` - An array of column definitions to be added to the table, and
+///               also to be audited.
+///
+/// # Errors
+///
+/// Returns any database errors.
+pub async fn create_audited_table<
+    'schema_manager,
+    TableIdenSource: IntoTableRef + Iden + IntoEnumIterator + Clone,
+    TableIdenAudit: IntoTableRef + Iden + IntoEnumIterator + Clone + 'static,
+    TableBuilder: Fn(TableKind, &mut TableCreateStatement),
+>(
+    manager: &SchemaManager<'schema_manager>,
+    table_iden_source: TableIdenSource,
+    table_iden_audit: TableIdenAudit,
+    table_builder: &TableBuilder,
+) -> Result<(), DbErr> {
+    create_table_helper(
+        manager,
+        table_iden_source.clone(),
+        table_builder,
+        TableKind::Source,
+    )
+    .await?;
+
+    create_table_helper(
+        manager,
+        table_iden_audit.clone(),
+        &|table_kind, table_create_statement| {
+            table_builder(table_kind, table_create_statement);
+            add_audit_columns_to_table::<TableIdenAudit>(table_create_statement);
+        },
+        TableKind::Audit,
+    )
+    .await?;
+
+    create_audit_trigger(manager, table_iden_source, table_iden_audit).await?;
+
+    Ok(())
+}
+
+async fn create_audit_trigger<
+    'schema_manager,
+    TableIdenSource: Iden + IntoEnumIterator,
+    TableIdenAudit: Iden,
+>(
+    manager: &SchemaManager<'schema_manager>,
+    table_iden_source: TableIdenSource,
+    table_iden_audit: TableIdenAudit,
+) -> Result<(), DbErr> {
+    let mut table_name_source = String::new();
+    table_iden_source.unquoted(&mut table_name_source);
+
+    let mut table_name_audit = String::new();
+    table_iden_audit.unquoted(&mut table_name_audit);
+
+    let column_names = TableIdenSource::iter()
+        .map(|x| {
+            let mut result = String::new();
+            x.unquoted(&mut result);
+            result
+        })
+        .filter(|x| *x != table_name_source)
+        .intersperse(", ".to_owned())
+        .collect::<String>();
+
+    let column_values = TableIdenSource::iter()
+        .map(|x| {
+            let mut result = String::new();
+            x.unquoted(&mut result);
+            result
+        })
+        .filter(|x| *x != table_name_source)
+        .map(|x| format!("CASE TG_OP WHEN 'INSERT' THEN NEW.{x} ELSE OLD.{x} END"))
+        .intersperse(", ".to_owned())
+        .collect::<String>();
+
+    manager
+        .get_connection()
+        .execute_unprepared(
+            format!(
+                r#"
+                    CREATE OR REPLACE FUNCTION function_audit_trigger_{table_name_source}()
+                    RETURNS TRIGGER AS $body$
+                        BEGIN
+                            INSERT INTO {table_name_audit} (
+                                {column_names},
+                                audit_action,
+                                audit_timestamp_transaction_start,
+                                audit_timestamp_statement_start,
+                                audit_timestamp_trigger,
+                                audit_client_host,
+                                audit_client_port,
+                                audit_client_query
+                            )
+                            VALUES (
+                                {column_values},
+                                CASE TG_OP
+                                    WHEN 'INSERT' THEN 'I'
+                                    WHEN 'UPDATE' THEN 'U'
+                                    WHEN 'DELETE' THEN 'D'
+                                    ELSE '?'
+                                END,
+                                current_timestamp,
+                                statement_timestamp(),
+                                clock_timestamp(),
+                                inet_client_addr(),
+                                inet_client_port(),
+                                current_query()
+                            );
+
+                            IF TG_OP = 'DELETE' THEN
+                                RETURN OLD;
+                            ELSE
+                                RETURN NEW;
+                            END IF;
+                        END
+                    $body$ LANGUAGE 'plpgsql';
+
+                    CREATE TRIGGER trigger_audit_{table_name_source}
+                    BEFORE INSERT OR UPDATE OR DELETE ON {table_name_source}
+                    FOR EACH ROW EXECUTE PROCEDURE function_audit_trigger_{table_name_source}();
+                "#,
+            )
+            .as_str(),
+        )
+        .await
+        .map(|_| ())
+}
+
+async fn create_table_helper<
+    'schema_manager,
+    TableIden: IntoTableRef,
+    TableBuilder: Fn(TableKind, &mut TableCreateStatement),
+>(
+    manager: &SchemaManager<'schema_manager>,
+    table_iden: TableIden,
+    table_builder: &TableBuilder,
+    table_kind: TableKind,
+) -> Result<(), DbErr> {
+    let mut source_table = Table::create();
+
+    source_table.table(table_iden).if_not_exists();
+
+    table_builder(table_kind, &mut source_table);
+
+    manager.create_table(source_table).await
+}
+
+fn add_audit_columns_to_table<TableEnum: IntoEnumIterator + Iden + 'static>(
+    table_create_statement: &mut TableCreateStatement,
+) {
+    table_create_statement
+        .col(
+            ColumnDef::new(
+                find_column_with_name::<TableEnum>("audit_id")
+                    .expect("required column 'audit_id' missing from audit table"),
+            )
+            .integer()
+            .not_null()
+            .auto_increment()
+            .primary_key(),
+        )
+        .col(
+            ColumnDef::new(
+                find_column_with_name::<TableEnum>("audit_action")
+                    .expect("required column 'audit_action' missing from audit table"),
+            )
+            .char()
+            .not_null(),
+        )
+        .col(
+            ColumnDef::new(
+                find_column_with_name::<TableEnum>("audit_timestamp_transaction_start").expect(
+                    "required column 'audit_timestamp_transaction_start' missing from audit table",
+                ),
+            )
+            .timestamp()
+            .not_null(),
+        )
+        .col(
+            ColumnDef::new(
+                find_column_with_name::<TableEnum>("audit_timestamp_statement_start").expect(
+                    "required column 'audit_timestamp_statement_start' missing from audit table",
+                ),
+            )
+            .timestamp()
+            .not_null(),
+        )
+        .col(
+            ColumnDef::new(
+                find_column_with_name::<TableEnum>("audit_timestamp_trigger")
+                    .expect("required column 'audit_timestamp_trigger' missing from audit table"),
+            )
+            .timestamp()
+            .not_null(),
+        )
+        .col(
+            ColumnDef::new(
+                find_column_with_name::<TableEnum>("audit_client_host")
+                    .expect("required column 'audit_client_host' missing from audit table"),
+            )
+            .string()
+            .not_null(),
+        )
+        .col(
+            ColumnDef::new(
+                find_column_with_name::<TableEnum>("audit_client_port")
+                    .expect("required column 'audit_client_port' missing from audit table"),
+            )
+            .integer()
+            .not_null(),
+        )
+        .col(
+            ColumnDef::new(
+                find_column_with_name::<TableEnum>("audit_client_query")
+                    .expect("required column 'audit_client_query' missing from audit table"),
+            )
+            .string()
+            .not_null(),
+        );
+}
+
+fn find_column_with_name<TableEnum: IntoEnumIterator + Iden>(name: &str) -> Option<TableEnum> {
+    TableEnum::iter().find(|x| -> bool {
+        let mut result = String::new();
+        x.unquoted(&mut result);
+        result == name
+    })
 }
--- core/db/migration/src/m20230218_120854_create_configuration_type_reference_table.rs
+++ core/db/migration/src/m20230218_120854_create_configuration_type_reference_table.rs
@@ -20,7 +20,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-use sea_orm_migration::{prelude::*, sea_orm::ConnectionTrait};
+use migration_common::{create_audited_table, TableKind};
+use sea_orm_migration::prelude::*;
 use strum_macros::EnumIter;
 
 #[derive(DeriveMigrationName)]
@@ -29,163 +30,45 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        manager
-            .create_table(
-                Table::create()
-                    .table(ConfigurationTypeReference::Table)
-                    .if_not_exists()
+        create_audited_table(
+            manager,
+            ConfigurationTypeReference::Table,
+            ConfigurationTypeReferenceAudit::Table,
+            &|table_kind: TableKind, table_create_statement: &mut TableCreateStatement| {
+                table_create_statement
                     .col(
-                        ColumnDef::new(ConfigurationTypeReference::Id)
-                            .integer()
-                            .not_null()
-                            .auto_increment()
-                            .primary_key(),
+                        match (
+                            &table_kind,
+                            ColumnDef::new(ConfigurationTypeReference::Id).integer(),
+                        ) {
+                            (TableKind::Source, x) => x.not_null().auto_increment().primary_key(),
+                            (TableKind::Audit, x) => x,
+                        },
                     )
                     .col(
-                        ColumnDef::new(ConfigurationTypeReference::Name)
-                            .string()
-                            .not_null()
-                            .unique_key(),
+                        match (
+                            &table_kind,
+                            ColumnDef::new(ConfigurationTypeReference::Name).string(),
+                        ) {
+                            (TableKind::Source, x) => x.not_null().unique_key(),
+                            (TableKind::Audit, x) => x,
+                        },
                     )
                     .col(
-                        ColumnDef::new(ConfigurationTypeReference::Description)
-                            .string()
-                            .not_null(),
+                        match (
+                            &table_kind,
+                            ColumnDef::new(ConfigurationTypeReference::Description).string(),
+                        ) {
+                            (TableKind::Source, x) => x.not_null(),
+                            (TableKind::Audit, x) => x,
+                        },
                     )
                     .col(
                         ColumnDef::new(ConfigurationTypeReference::DeactivateTimestamp).timestamp(),
-                    )
-                    .to_owned(),
-            )
-            .await?;
-
-        manager
-            .create_table(
-                Table::create()
-                    .table(ConfigurationTypeReferenceAudit::Table)
-                    .if_not_exists()
-                    .col(
-                        ColumnDef::new(ConfigurationTypeReferenceAudit::AuditId)
-                            .integer()
-                            .not_null()
-                            .auto_increment()
-                            .primary_key(),
-                    )
-                    .col(ColumnDef::new(ConfigurationTypeReferenceAudit::Id).integer())
-                    .col(ColumnDef::new(ConfigurationTypeReferenceAudit::Name).string())
-                    .col(ColumnDef::new(ConfigurationTypeReferenceAudit::Description).string())
-                    .col(
-                        ColumnDef::new(ConfigurationTypeReferenceAudit::DeactivateTimestamp)
-                            .timestamp(),
-                    )
-                    .col(
-                        ColumnDef::new(ConfigurationTypeReferenceAudit::AuditAction)
-                            .char()
-                            .not_null(),
-                    )
-                    .col(
-                        ColumnDef::new(
-                            ConfigurationTypeReferenceAudit::AuditTimestampTransactionStart,
-                        )
-                        .timestamp()
-                        .not_null(),
-                    )
-                    .col(
-                        ColumnDef::new(
-                            ConfigurationTypeReferenceAudit::AuditTimestampStatementStart,
-                        )
-                        .timestamp()
-                        .not_null(),
-                    )
-                    .col(
-                        ColumnDef::new(ConfigurationTypeReferenceAudit::AuditTimestampTrigger)
-                            .timestamp()
-                            .not_null(),
-                    )
-                    .col(
-                        ColumnDef::new(ConfigurationTypeReferenceAudit::AuditClientHost)
-                            .string()
-                            .not_null(),
-                    )
-                    .col(
-                        ColumnDef::new(ConfigurationTypeReferenceAudit::AuditClientPort)
-                            .integer()
-                            .not_null(),
-                    )
-                    .col(
-                        ColumnDef::new(ConfigurationTypeReferenceAudit::AuditClientQuery)
-                            .string()
-                            .not_null(),
-                    )
-                    .to_owned(),
-            )
-            .await?;
-
-        manager
-            .get_connection()
-            .execute_unprepared(
-                r#"
-                    CREATE OR REPLACE FUNCTION function_audit_trigger_configuration_type_reference()
-                    RETURNS TRIGGER AS $body$
-                        BEGIN
-                            INSERT INTO configuration_type_reference_audit (
-                                id,
-                                name,
-                                description,
-                                deactivate_timestamp,
-                                audit_action,
-                                audit_timestamp_transaction_start,
-                                audit_timestamp_statement_start,
-                                audit_timestamp_trigger,
-                                audit_client_host,
-                                audit_client_port,
-                                audit_client_query
-                            )
-                            VALUES (
-                                CASE TG_OP
-                                    WHEN 'INSERT' THEN NEW.id
-                                    ELSE OLD.id
-                                END,
-                                CASE TG_OP
-                                    WHEN 'INSERT' THEN NEW.name
-                                    ELSE OLD.name
-                                END,
-                                CASE TG_OP
-                                    WHEN 'INSERT' THEN NEW.description
-                                    ELSE OLD.description
-                                END,
-                                CASE TG_OP
-                                    WHEN 'INSERT' THEN NEW.deactivate_timestamp
-                                    ELSE OLD.deactivate_timestamp
-                                END,
-                                CASE TG_OP
-                                    WHEN 'INSERT' THEN 'I'
-                                    WHEN 'UPDATE' THEN 'U'
-                                    WHEN 'DELETE' THEN 'D'
-                                    ELSE '?'
-                                END,
-                                current_timestamp,
-                                statement_timestamp(),
-                                clock_timestamp(),
-                                inet_client_addr(),
-                                inet_client_port(),
-                                current_query()
-                            );
-
-                            IF TG_OP = 'DELETE' THEN
-                                RETURN OLD;
-                            ELSE
-                                RETURN NEW;
-                            END IF;
-                        END
-                    $body$ LANGUAGE 'plpgsql';
-
-                    CREATE TRIGGER trigger_audit_configuration_type_reference
-                    BEFORE INSERT OR UPDATE OR DELETE ON configuration_type_reference
-                    FOR EACH ROW EXECUTE PROCEDURE function_audit_trigger_configuration_type_reference();
-                "#,
-            )
-            .await?;
+                    );
+            },
+        )
+        .await?;
 
         Ok(())
     }
@@ -212,7 +95,7 @@ impl MigrationTrait for Migration {
 }
 
 /// Learn more at https://docs.rs/sea-query#iden
-#[derive(Iden, EnumIter)]
+#[derive(Iden, EnumIter, Clone)]
 pub enum ConfigurationTypeReference {
     Table,
     Id,
@@ -221,7 +104,7 @@ pub enum ConfigurationTypeReference {
     DeactivateTimestamp,
 }
 
-#[derive(Iden, EnumIter)]
+#[derive(Iden, EnumIter, Clone)]
 pub enum ConfigurationTypeReferenceAudit {
     Table,
     AuditId,
@@ -246,6 +129,8 @@ mod tests {
     #[test]
     fn test_audit_table_columns() {
         assert_audit_table_enum_valid::<ConfigurationTypeReference, ConfigurationTypeReferenceAudit>(
+            "configuration_type_reference",
+            "configuration_type_reference_audit",
         );
     }
 }
